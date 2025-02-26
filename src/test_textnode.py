e0a8f4e5dc8e02288c0dde845f85d88a828b12f6
addelian <addelia.nicolas@gmail.com>
2025-02-26 12:48:42 -0600

    add tests

---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    
+    def test_not_eq(self):
+        node = TextNode("Check me", TextType.NORMAL)
+        node_b = TextNode("Check me", TextType.BOLD)
+        self.assertEqual(node.text, node_b.text)
+        self.assertNotEqual(node, node_b)
+    
+    
+    def test_url(self):
+        node = TextNode("New node", TextType.ITALIC, "https://plots.club")
+        self.assertEqual(node.url, "https://plots.club")
+
+
+    def test_default_url(self):
+        node = TextNode("Empty Earl", TextType.CODE)
+        node_explicit = TextNode("REALLY Empty Earl", TextType.CODE, None)
+        self.assertEqual(node.url, None)
+        self.assertEqual(node_explicit.url, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,15 +1,13 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = "normal"
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
 
 class TextNode:
-    url = None
-
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
