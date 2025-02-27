e33c17042021d61cdbfa978dd3e329ea35948c58
addelian <addelia.nicolas@gmail.com>
2025-02-26 21:45:16 -0600

    update tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,18 +6,18 @@ from htmlnode import HTMLNode
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("a", "Click me", None, {"href": "https://www.google.com"})
-        self.assertEqual(node.__repr__(), "HTMLNode(a, Click me, None, {'href': 'https://www.google.com'})")
+        self.assertEqual(node.__repr__(), "HTMLNode(a, Click me, children: None, {'href': 'https://www.google.com'})")
 
     
     def test_defaults(self):
         node = HTMLNode()
-        self.assertEqual(node.__repr__(), "HTMLNode(None, None, None, None)")
+        self.assertEqual(node.__repr__(), "HTMLNode(None, None, children: None, None)")
     
     
     def test_props_to_html(self):
         node = HTMLNode("a", "Click me", None, {"href": "https://www.google.com", "target": "_blank"})
         print(node.to_html())
-        self.assertEqual(node.to_html(), "href=https://www.google.com target=_blank ")
+        self.assertEqual(node.to_html(), " href=https://www.google.com target=_blank")
 
 
 if __name__ == "__main__":
