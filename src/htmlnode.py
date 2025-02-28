5a690292016879ce1211232723b77bf847dd99ef
addelian <addelia.nicolas@gmail.com>
2025-02-28 09:06:18 -0600

    add ParentNode, add tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -38,3 +38,23 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        child_list = ""
+        for child in self.children:
+            child_list += child.to_html()
+        return f"<{self.tag}>{child_list}</{self.tag}>"
+    
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -33,6 +33,41 @@ class TestHTMLNode(unittest.TestCase):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
 
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+
+    def test_to_html_with_no_children(self):
+        childless_node = ParentNode("p", [])
+        self.assertEqual(
+            childless_node.to_html(),
+            "<p></p>"
+        )
+
+    
+    def test_to_html_with_multiple_children(self):
+        child1 = LeafNode("span", "I'm a baby")
+        child2 = LeafNode(None, "hear me roar")
+        child3 = LeafNode("em", "STRONGBOI")
+        child4 = LeafNode("i", "I'm going to betray everyone")
+        busy_parent = ParentNode("h3", [child1, child2, child3, child4])
+        self.assertEqual(
+            busy_parent.to_html(),
+            "<h3><span>I'm a baby</span>hear me roar<em>STRONGBOI</em><i>I'm going to betray everyone</i></h3>"
+        )
 
 if __name__ == "__main__":
     unittest.main()
