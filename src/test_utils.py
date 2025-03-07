1bac8de0d89d5a7d5da6e9cf337dec89afa0e2ac
addelian <addelia.nicolas@gmail.com>
2025-03-07 17:35:43 -0600

    combine splits to handle combined inline md nodes, add tests

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from utils import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from utils import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestTextNodeToHTMLNode(unittest.TestCase):    
     def test_text(self):
@@ -391,6 +391,42 @@ class TestSplitNodesLink(unittest.TestCase):
             ],
             new_nodes,
         ) 
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_combined_node(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            new_nodes,
+        )
+    
+
+    def test_no_md_node(self):
+        text = "This is a simple text block"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [TextNode("This is a simple text block", TextType.TEXT)], new_nodes
+        )
+
+    
+    def test_combined_block_some_invalid_md(self):
+        text = "This is **text** with an _italic_ word and an `invalid code block and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        with self.assertRaises(ValueError):
+            text_to_textnodes(text)
+
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -22,20 +22,24 @@ def text_node_to_html_node(text_node: TextNode):
             raise ValueError(f"invalid text type: {text_node.text_type}")
         
 
-def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        node_parts = old_node.text.split(delimiter)
-        if len(node_parts) != 3:
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
             raise ValueError("invalid markdown syntax - do you have one too few or too many delimiters?")
-        new_nodes.extend([
-            TextNode(node_parts[0], TextType.TEXT),
-            TextNode(node_parts[1], text_type),
-            TextNode(node_parts[2], TextType.TEXT)
-        ])
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
 
 
@@ -49,6 +53,9 @@ def extract_markdown_links(text):
 
 def parse_node(node: TextNode, extract_func, text_type: TextType, ep):
     nodes = []
+    if node.text_type != TextType.TEXT:
+        nodes.append(node)
+        return nodes
     extracted_params = extract_func(node.text)
     if extracted_params == []:
         nodes.append(node)
@@ -80,4 +87,20 @@ def split_nodes_link(old_nodes: list[TextNode]):
     new_nodes = []
     for node in old_nodes:
         new_nodes.extend(parse_node(node, extract_markdown_links, TextType.LINK, ''))
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    new_nodes = split_nodes_link(
+        split_nodes_image(
+            split_nodes_delimiter(
+                split_nodes_delimiter(
+                    split_nodes_delimiter(
+                        [node], "`", TextType.CODE
+                        ), "**", TextType.BOLD
+                    ), "_", TextType.ITALIC
+                )
+            )
+        )
     return new_nodes
